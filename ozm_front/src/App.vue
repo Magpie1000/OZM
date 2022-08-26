a7a56296bc8e431a71fae40dafe7bf6f4449edeb
Magpie1000 <web.1000c@gmail.com>
2022-08-26 17:17:41 +0900

    BUILD: 🏹 Vue Router added

--- ozm_front/src/App.vue
+++ ozm_front/src/App.vue
@@ -1,26 +1,32 @@
 <template>
-  <v-app>
-    <TheNavBar />
-    <v-main>
-      <LandingPage />
-    </v-main>
-  </v-app>
+  <div id="app">
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    <router-view/>
+  </div>
 </template>
 
-<script>
-import TheNavBar from './components/bar/TheNavBar.vue';
-import LandingPage from './views/LandingPage.vue';
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-export default {
-  name: 'App',
+nav {
+  padding: 30px;
 
-  components: {
-    TheNavBar,
-    LandingPage,
-  },
+  a {
+    font-weight: bold;
+    color: #2c3e50;
 
-  data: () => ({
-    //
-  }),
-};
-</script>
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>

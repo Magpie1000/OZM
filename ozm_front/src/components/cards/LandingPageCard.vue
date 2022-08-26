b1ae680f41b810a67a8d13bc33be59fbff732c08
Magpie1000 <web.1000c@gmail.com>
2022-08-26 17:14:58 +0900

    FEAT: 🎸 Landing page created

---
+++ ozm_front/src/components/cards/LandingPageCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-card class="ma-1">
+    <v-card-title>{{ title }}</v-card-title>
+    <v-img
+      :src="imgURL"
+      contain
+      max-height="300"
+    />
+    <v-card-text>{{ content }}</v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'LandingPageCard',
+  
+  props: {
+    title: String,
+    imgURL: String,
+    content: String,
+  },
+
+  data: () => ({
+    //
+  }),
+}
+</script>
+
+<style>
+
+</style>

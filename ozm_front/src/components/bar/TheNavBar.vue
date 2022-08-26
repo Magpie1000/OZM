5c7cf7292947636bbf32667635e7b3140a2e4289
Magpie1000 <web.1000c@gmail.com>
2022-08-26 17:14:39 +0900

    FEAT: 🎸 Navbar divided into component

---
+++ ozm_front/src/components/bar/TheNavBar.vue
@@ -0,0 +1,49 @@
+<template>
+    <v-app-bar
+      app
+      color="dark"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-icon class="mx-2">
+          mdi-noodles
+        </v-icon>
+        <v-toolbar-title>멀캠 오점무</v-toolbar-title>
+        <p></p>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+  <!-- <div>
+    <p>111</p>
+  </div> -->
+</template>
+
+<script>
+export default {
+  name: 'TheNavBar',
+  
+  components: {
+
+  },
+
+  data: () => ({
+
+  }),
+  
+}
+</script>
+
+<style>
+
+</style>
